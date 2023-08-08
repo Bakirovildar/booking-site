8c38c2189e44c1f4086dfc95949a3e393dea9993
Bakirovildar <bakirovildar2011@mail.ru>
2023-08-08 18:24:04 +0500

    адаптив

--- src/shared/Main/MainImage/MainImage.tsx
+++ src/shared/Main/MainImage/MainImage.tsx
@@ -4,7 +4,7 @@ export const MainImage = () => {
     return (
         <div className='main-image-container'>
             <div className='main-image'>
-                <img src="https://images.unsplash.com/photo-1470259078422-826894b933aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1174&q=80" alt=""/>
+                <img src="https://images.unsplash.com/photo-1516975080664-ed2fc6a32937?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt=""/>
             </div>
         </div>
     )
